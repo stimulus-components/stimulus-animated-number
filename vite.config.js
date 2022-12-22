feac33ee46abe7612ddbfdfbd15327243aad8436
Guillaume Briday <guillaumebriday@gmail.com>
2022-12-22 14:01:12 -0500

    Using Netlify mode

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'StimulusAnimatedNumber',
-      fileName: 'stimulus-animated-number'
-    },
-    rollupOptions: {
-      external: ['@hotwired/stimulus'],
-      output: {
-        globals: {
-          '@hotwired/stimulus': 'Stimulus'
+export default defineConfig(({ mode }) => {
+  if (mode === 'netlify') {
+    return {}
+  }
+
+  return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'StimulusAnimatedNumber',
+        fileName: 'stimulus-animated-number'
+      },
+      rollupOptions: {
+        external: ['@hotwired/stimulus'],
+        output: {
+          globals: {
+            '@hotwired/stimulus': 'Stimulus'
+          }
         }
       }
     }
